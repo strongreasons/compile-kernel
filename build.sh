3f126f8689162ae4ba217e7d7575e45447870d44
strongreasons <109900777+strongreasons@users.noreply.github.com>
2023-05-07 21:07:25 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -64,9 +64,9 @@ KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel sourc
 export LD=ld.lld
 export KBUILD_BUILD_USER=queen # Change with your own name or else.
 IMAGE=$(pwd)/$DEVICE_CODENAME/out/arch/arm64/boot/Image.gz-dtb
-CLANG_VER="$("$ClangPath"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+#CLANG_VER="$("$ClangPath"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 #LLD_VER="$("$ClangPath"/bin/ld.lld --version | head -n 1)"
-export KBUILD_COMPILER_STRING="$CLANG_VER x GCC 4.9"
+#export KBUILD_COMPILER_STRING="$CLANG_VER x GCC 4.9"
 DATE=$(date +"%F-%S")
 START=$(date +"%s")
 
