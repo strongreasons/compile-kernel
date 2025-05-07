4f5238625ded34789b81fa84ba6d65d86575634e
Kneba <abenkenary3@gmail.com>
2025-05-08 02:39:08 +0700

    Update DATE

--- build.sh
+++ build.sh
@@ -164,9 +164,9 @@ function finerr() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 $KERNELNAME-$VARIANT-$KERVER-"$DATE2" * -x .git README.md ./*placeholder anykernel-real.sh .gitignore  zipsigner* *.zip
+    zip -r9 $KERNELNAME-$VARIANT-$KERVER-"$DATE" * -x .git README.md ./*placeholder anykernel-real.sh .gitignore  zipsigner* *.zip
 
-    ZIP_FINAL="$KERNELNAME-$VARIANT-$KERVER-$DATE2"
+    ZIP_FINAL="$KERNELNAME-$VARIANT-$KERVER-$DATE"
 
     msg "|| Signing Zip ||"
     tg_post_msg "<code>🔑 Signing Zip file with AOSP keys..</code>"
