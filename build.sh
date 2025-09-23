d5eeb6b1beec17e2761b71f9399bd276da862e72
strongreasons <abenkenari@gmail.com>
2025-09-24 00:45:59 +0700

    reup

--- build.sh
+++ build.sh
@@ -32,7 +32,8 @@ VERSION=CLO
 VARIANT=HMP
 
 # Clone Kernel Source
-git clone --depth=1 --recursive https://$AWAL:$AKHIR@github.com/Tiktodz/android_kernel_asus_sdm636 -b clotzy --single-branch $DEVICE_CODENAME
+git clone --depth=1 --recursive https://$AWAL:$AKHIR@github.com/strongreasons/android_kernel_asus_sdm660 -b clo --single-branch $DEVICE_CODENAME
+#git clone --depth=1 --recursive https://$AWAL:$AKHIR@github.com/Tiktodz/android_kernel_asus_sdm636 -b clotzy --single-branch $DEVICE_CODENAME
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
