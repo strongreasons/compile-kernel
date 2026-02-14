3b94554fdfa0f8a0a0bde4631fb9eb815241bcc5
texascake <tiktodz@soulvibe-team.cloud>
2026-02-14 18:33:13 +0700

    Update build.sh 1833

--- build.sh
+++ build.sh
@@ -27,12 +27,12 @@ GCCaPath="${MainPath}/GCC64"
 GCCbPath="${MainPath}/GCC32"
 
 # Identity
-KERNELNAME=TOM
+KERNELNAME=TOM-TZY
 VARIANT=HMP
 VERSION=CLO
 
 # Clone Kernel Source
-git clone --depth=1 --recursive https://$AWAL:$AKHIR@github.com/strongreasons/android_kernel_asus_sdm660 -b folk --single-branch $DEVICE_CODENAME
+git clone --depth=1 --recurse-submodules https://$AWAL:$AKHIR@github.com/strongreasons/android_kernel_asus_sdm660 -b folk --single-branch $DEVICE_CODENAME
 #git clone --depth=1 --recursive https://$AWAL:$AKHIR@github.com/Tiktodz/android_kernel_asus_sdm636 -b clotzy --single-branch $DEVICE_CODENAME
 
 # Show manufacturer info
