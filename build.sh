bbaeb206505c199704a883e97590bf2f11cc8beb
strongreasons <strongreasons@users.noreply.github.com>
2026-03-03 15:37:17 +0000

    Switch to hookless
    Signed-off-by: strongreasons <strongreasons@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ VARIANT=HMP
 VERSION=CLO
 
 # Clone Kernel Source
-git clone --depth=1 --recurse-submodules https://$AWAL:$AKHIR@github.com/strongreasons/android_kernel_asus_sdm660 -b folk --single-branch $DEVICE_CODENAME
+git clone --depth=1 https://$AWAL:$AKHIR@github.com/strongreasons/android_kernel_asus_sdm660 -b folk --single-branch $DEVICE_CODENAME
 #git clone --depth=1 --recursive https://$AWAL:$AKHIR@github.com/Tiktodz/android_kernel_asus_sdm636 -b clotzy --single-branch $DEVICE_CODENAME
 
 # Show manufacturer info
@@ -90,6 +90,7 @@ tg_post_msg() {
 # Compile time
 compile(){
 cd ${KERNEL_ROOTDIR}
+curl -LSs "https://raw.githubusercontent.com/Sorayukii/KernelSU-Next/stable/kernel/setup.sh" | bash -s hookless
 # Additional command (if you're lazy to commit :v)
 sed -i 's/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION="-TOM-969+"/g' arch/arm64/configs/$KERNEL_DEFCONFIG
 export HASH_HEAD=$(git rev-parse --short HEAD)
