2d931f74b6e7f22e9e7d59d659b422931aa9f101
strongreasons <strongreasons@users.noreply.github.com>
2026-03-28 02:31:39 +0000

    build: back to prev commit
    Signed-off-by: strongreasons <strongreasons@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ VARIANT=HMP
 VERSION=CLO
 
 # Clone Kernel Source
-git clone --depth=1 https://$AWAL:$AKHIR@github.com/strongreasons/android_kernel_asus_sdm660 -b hook --single-branch $DEVICE_CODENAME
+git clone --depth=1 https://$AWAL:$AKHIR@github.com/strongreasons/android_kernel_asus_sdm660 -b kowsu --single-branch $DEVICE_CODENAME
 #git clone --depth=1 --recursive https://$AWAL:$AKHIR@github.com/Tiktodz/android_kernel_asus_sdm636 -b clotzy --single-branch $DEVICE_CODENAME
 
 # Show manufacturer info
