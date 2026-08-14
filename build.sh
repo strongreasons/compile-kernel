c57de4f639274aa49bdba90d9d64e8a111da46b4
strongreasons <abenkenari@gmail.com>
2026-08-14 19:55:55 +0700

    fix VERSION name

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ GCCbPath="${MainPath}/GCC32"
 KERNELNAME=TOM
 KERNEL_DEFCONFIG=vendor/asus/X00TD_defconfig
 VARIANT=HMP
-VERSION=EOL
+VERSION=CLO
 
 # Clone Kernel Source
 #git clone --depth=1 https://$AWAL:$AKHIR@github.com/strongreasons/android_kernel_asus_sdm660 -b r44 --single-branch $DEVICE_CODENAME
